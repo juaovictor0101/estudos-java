27d0fbf7811a458be45e2c7534c887929f2f8562
João <profissionaljoao1101@gmail.com>
2025-04-23 15:59:33 -0300

    feat: adcionando maven ao projeto e iniciando os estudos em JDBC.

---
+++ src/main/java/javaviradonojiraya/javacore/ZZIjdbc/conn/ConnectionFactory.java
@@ -0,0 +1,6 @@
+package javaviradonojiraya.javacore.ZZIjdbc.conn;
+
+public enum ConnectionFactory {
+    //java.sql = Connection, Statement, ResultSet, DriveManager -> interfaces para conexão com DB
+
+}
