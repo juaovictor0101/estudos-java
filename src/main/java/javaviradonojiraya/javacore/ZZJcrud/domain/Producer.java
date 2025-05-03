4294e5036df5faac110c7fea2539fe070a7ed426
João Victor <profissionaljoao1101@gmail.com>
2025-05-02 21:28:07 -0300

    feat: finalizando o conteúdo do curso com um CRUD num DB relacional.

---
+++ src/main/java/javaviradonojiraya/javacore/ZZJcrud/domain/Producer.java
@@ -0,0 +1,12 @@
+package javaviradonojiraya.javacore.ZZJcrud.domain;
+
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder
+
+public class Producer {
+    Integer id;
+    String name;
+}
---
+++ src/main/java/javaviradonojiraya/javacore/ZZJcrud/test/CrudTest01.java
@@ -0,0 +1,28 @@
+package javaviradonojiraya.javacore.ZZJcrud.test;
+
+import javaviradonojiraya.javacore.ZZJcrud.service.ProducerService;
+
+import java.util.Scanner;
+
+public class CrudTest01 {
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int op;
+        while (true){
+            producerMenu();
+            op = Integer.parseInt(SCANNER.nextLine());
+            if(op == 0) break;
+            ProducerService.menu(op);
+        }
+
+    }
+    private static void producerMenu(){
+        System.out.println("Type the number of your operation ");
+        System.out.println("1. Search for Producer");
+        System.out.println("2. Delete Producer");
+        System.out.println("3. Save Producer");
+        System.out.println("4. Update Producer");
+        System.out.println("0. exist");
+    }
+}
